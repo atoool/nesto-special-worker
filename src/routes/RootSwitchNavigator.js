21eff30ba28c09f8f60343f4d8885c6f51892f66
Abhijith P <abhijith@riafy.me>
2021-05-03 21:42:37 +0530

    Maintenance mode alert and app update alert added

--- src/routes/RootSwitchNavigator.js
+++ src/routes/RootSwitchNavigator.js
@@ -1,45 +1,80 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState, useEffect} from 'react';
+import {Alert, Linking} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
+import remoteConfig from '@react-native-firebase/remote-config';
+
 import SplashScreen from '../screens/SplashScreen';
 import LoginScreen from '../screens/LoginScreen';
 import TabsNavigator from './TabsNavigator';
 import {AuthContext} from '../context/AuthContext';
 import {WorkerContextProvider} from '../context/WorkerContext';
+import {version} from '../../package.json';
 
 const Stack = createStackNavigator();
 
 const RootSwitchNavigator = () => {
   const {authStateLoading, userType} = useContext(AuthContext);
-  if (authStateLoading) {
-    return (
-      <Stack.Navigator initialRouteName="SplashScreen">
-        <Stack.Screen
-          name="SplashScreen"
-          component={SplashScreen}
-          options={{headerShown: false}}
-        />
-      </Stack.Navigator>
+  const [configLoading, setConfigLoading] = useState(true);
+  const [maintenanceMode, setMaintenanceMode] = useState(false);
+  const [updateAvailable, setUpdateAvailable] = useState(false);
+
+  useEffect(() => {
+    fetchRemoteConfig();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const fetchRemoteConfig = async () => {
+    await remoteConfig().fetch(0);
+    await remoteConfig().activate();
+    const _maintenanceMode =
+      remoteConfig().getString('specialWorkerMaintenanceMode') === 'true';
+    const minVersion = remoteConfig().getString('specialWorkerMinVersion');
+    const _updateAvailable = version < minVersion;
+    setUpdateAvailable(_updateAvailable);
+    if (_maintenanceMode) {
+      Alert.alert('', 'Server under maintenance. Please try again later');
+    } else if (_updateAvailable) {
+      Alert.alert(
+        '',
+        'Please update app to continue',
+        [{text: 'Update', onPress: handleAppUpdate}],
+        {cancelable: false},
+      );
+    }
+    setMaintenanceMode(_maintenanceMode ?? false);
+    setConfigLoading(false);
+  };
+
+  const handleAppUpdate = () =>
+    Linking.openURL(
+      'https://play.google.com/store/apps/details?id=com.nesto.specialworker',
     );
-  } else if (
-    userType.toLowerCase() === 'fishmonger' ||
-    userType.toLowerCase() === 'butcher'
-  ) {
-    return (
-      <WorkerContextProvider>
-        <TabsNavigator />
-      </WorkerContextProvider>
-    );
-  } else {
-    return (
-      <Stack.Navigator initialRouteName="LoginScreen">
-        <Stack.Screen
-          name="LoginScreen"
-          component={LoginScreen}
-          options={{headerShown: false}}
-        />
-      </Stack.Navigator>
-    );
-  }
+
+  return authStateLoading ||
+    configLoading ||
+    maintenanceMode ||
+    updateAvailable ? (
+    <Stack.Navigator initialRouteName="SplashScreen">
+      <Stack.Screen
+        name="SplashScreen"
+        component={SplashScreen}
+        options={{headerShown: false}}
+      />
+    </Stack.Navigator>
+  ) : userType.toLowerCase() === 'fishmonger' ||
+    userType.toLowerCase() === 'butcher' ? (
+    <WorkerContextProvider>
+      <TabsNavigator />
+    </WorkerContextProvider>
+  ) : (
+    <Stack.Navigator initialRouteName="LoginScreen">
+      <Stack.Screen
+        name="LoginScreen"
+        component={LoginScreen}
+        options={{headerShown: false}}
+      />
+    </Stack.Navigator>
+  );
 };
 
 export default RootSwitchNavigator;
